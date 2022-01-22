b788e098d48798ed1e504ef925bf8b69bd28e127
dilsoncampelo10 <dilsono316@gmail.com>
2022-01-22 11:48:10 -0300

    Parte um- page principal

---
+++ js/slide.js
@@ -0,0 +1,41 @@
+let imagens = [];
+let slide;
+let posicao;
+let tamanho;
+let tempo;
+
+//Adiciona as imagens em um vetor
+function precarregar(){
+    for(let i=0;i<6;i++){
+        imagens[i] = new Image();
+        imagens[i].src = `images/i${i}.jpg`;
+    }
+}
+
+//Renderiza a imagem na tela
+function carregarimagem(pos){
+    slide = document.getElementsByClassName('slider');
+    slide[0].style.backgroundImage = `url('${imagens[pos].src}')`;
+}
+
+function troca(dir){
+    posicao += dir;
+    if(posicao>tamanho-1){
+        posicao = 0;
+    } 
+    if(posicao<0){
+        posicao = tamanho-1;
+    }
+    carregarimagem(posicao);
+}
+window.onload = function(){
+    precarregar();
+    posicao = 0;
+    tamanho = imagens.length;
+    tempo = 6000;
+    carregarimagem(posicao);
+    setInterval(function(){
+        troca(1);
+    }, tempo);
+    
+}
