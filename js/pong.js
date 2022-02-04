1fd0cb3722b4946a17f08c0910d8a1de705516b5
dilsoncampelo10 <dilsono316@gmail.com>
2022-02-04 12:23:00 -0300

    Movimentacao Pong

--- js/pong.js
+++ js/pong.js
@@ -12,21 +12,56 @@ let diryJogador, diryCpu, dirYBola, dirxBola;
 
 let velJogador, velCpu, velBola;
 
-let frames;
+let frames, jogando=false;
+
+//Altura de elementos
+let hBarra = 140, hCampo = 400;
 
 function teclaDown(event){
-    if(event.key==''){
-
+    if(event.key=='ArrowUp'){
+        diryJogador = -1;
+    } else if(event.key=='ArrowDown'){
+        diryJogador = 1;
     }
-    console.log(event);
 }
 
-function teclaUp(){
-
+function teclaUp(event){
+    if(event.key=='ArrowUp'){
+        diryJogador = 0;
+    } else if(event.key = 'ArrowDown'){
+        diryJogador = 0;
+    }
 }
 
+function controlaJogador(){
+        posyJogador += diryJogador * velJogador;
+        if(posyJogador+hBarra>=hCampo || posyJogador<0){
+            posyJogador += diryJogador * velJogador *(-1);
+        }
+        
+        jogador.style.top = posyJogador+'px';
+}
+
+function game(){
+    if(jogando){
+        controlaJogador();
+    }
+    requestAnimationFrame(game);
+}
 function iniciarJogo(){
-
+    if(!jogando){
+        jogando = true;
+        diryJogador = 0;
+        posyJogador = 138;
+        velJogador = 5;
+        document.addEventListener('keydown', function(event){
+            teclaDown(event);
+        });
+        document.addEventListener('keyup',function(event){
+            teclaUp(event);
+        });
+        game();
+    }
 }
 
-document.addEventListener('keydown', teclaDown);
+btniniciar.addEventListener('click',iniciarJogo);
